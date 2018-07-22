a85da6cd0b1f311ebc84c544c58e2020555e493b
Klemen Tušar <techouse@gmail.com>
2018-07-22 11:51:30 +0200

    Detect horizontal planes in the real world

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // display yellow feature points on the screen while it's trying to detect a plane
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -40,20 +43,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        }
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//            sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration.isSupported
-            ? ARWorldTrackingConfiguration() // iPhone 6s and newer models only!
-            : AROrientationTrackingConfiguration() // iPhone 5 and 6
+        let configuration = ARWorldTrackingConfiguration() // iPhone 6s and newer models only!
+        
+        configuration.planeDetection = .horizontal // only world tracking supports this
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -64,5 +67,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    // run this once a plane is detected
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            print("plane detected")
+            
+            // cast the ARAnchor to an ARPlaneAnchor
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            // an SCNPlane is by default a vertical plane
+            // check the docs https://developer.apple.com/documentation/scenekit/scnplane
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            // because the above SCNPlane is by default vertical we have to rotate it so that it becomes horizontal
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0) // the rotation angle is in counter clokwise radians
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
 }
